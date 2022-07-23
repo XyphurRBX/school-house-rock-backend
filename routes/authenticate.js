e666a338f5830e487f69e848ea9081f8c52b59a7
Xyphur <xyphurrbx@gmail.com>
2022-07-23 18:30:20 -0500

    change var to const

--- routes/authenticate.js
+++ routes/authenticate.js
@@ -1,5 +1,5 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
 
 router.post('/', function(req, res, next) {
 // Actual implementation would check values in a database

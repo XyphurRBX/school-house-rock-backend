98be92f197e357116926a5aa505d6d0950be95c8
Xyphur <xyphurrbx@gmail.com>
2022-07-23 18:30:10 -0500

    add db service

---
+++ services/db.js
@@ -0,0 +1,14 @@
+const { Pool } = require('pg');
+
+const connectionPool = new Pool();
+
+
+async function getUserPassword(username) {
+
+}
+
+async function setUserPassword(username, password) {
+
+}
+
+module.exports = { getUserPassword, setUserPassword };

45074cf2daffc729b56c1e4404ee894ba167e056
Xyphur <xyphurrbx@gmail.com>
2022-07-23 17:50:02 -0500

    add dotenv module

--- app.js
+++ app.js
@@ -1,3 +1,6 @@
+const dotenv = require('dotenv');
+dotenv.config();
+
 const express = require('express');
 const cookieParser = require('cookie-parser');
 const session = require('express-session')

3100fa2840b436668306597dbad1889d870f5399
Xyphur <xyphurrbx@gmail.com>
2022-07-30 15:34:11 -0500

    remove debug prints

--- services/db.js
+++ services/db.js
@@ -35,10 +35,7 @@ const orderByQueryDescending = "SELECT * FROM %I ORDER BY (VALUES(%L)) DESC LIMI
 const noOrderByQuery = "SELECT * FROM %I LIMIT %L OFFSET %L";
 async function getTable(tableName, limit, offset, orderBy, ascending) {
 	try {
-		console.log(orderBy != "");
-		console.log(!!orderBy);
 		if (orderBy != "" && (!!orderBy)) {
-			console.log("Ordering")
 			if (ascending)
 				return await connectionPool.query(format(orderByQueryAscending, tableName, orderBy, limit, offset));
 			else
@@ -47,7 +44,6 @@ async function getTable(tableName, limit, offset, orderBy, ascending) {
 			return await connectionPool.query(format(noOrderByQuery, tableName, limit, offset));
 		}
 	} catch (err) {
-		console.log(err);
 		return null;
 	}
 }
